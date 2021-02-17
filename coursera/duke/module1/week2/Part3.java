16f5ce05dc4bf961517485c1ed8671b4f069597a
Maloy Wilkes <Juxtaposedwords@gmail.com>
2021-02-16 19:58:48 -0800

    Adding test for LastPart() which looks for a suffix.

--- coursera/duke/module1/week2/Part3.java
+++ coursera/duke/module1/week2/Part3.java
@@ -18,9 +18,63 @@ public class  Part3 {
 		return corpus;
 	}
 	public void testing(){
+		testLastPart();
 
 	}
+	class testCase {
+        public String description;
+        public String corpus;
+        public String subject;
+        public String want;
+        testCase(String description, String corpus, String subject, String want){
+            this.description = description;
+            this.corpus = corpus;
+            this.subject= subject;
+            this.want = want;
+        }
+    }
+    class testLast {
+        public String description;
+        public String corpus;
+        public String subject;
+        public boolean want;
+        testCase(String description, String corpus, String subject, boolean want){
+            this.description = description;
+            this.corpus = corpus;
+            this.subject= subject;
+            this.want = want;
+        }
+    }
 	public void testLastPart(){
+		 testCase[] tests = new testCase[] {
+        new testLast( 
+            "subject exist in corpus many times",
+            "bodybody"
+            "dy",
+            true),
+        new testLast(
+            "subject not in corpus",
+            "bodybody",
+            "flegm",
+            false),
+        new testLast(
+            "subject exists in corpus once",
+            "ayebody",
+            "dy",
+            true)};
+        for ( testLast tc: tests ) {
+            System.out.printf("Test %s:\n",tc.description);
+            System.out.printf("  Status:");
+            boolean got =  lastPart(tc.corpus.tc.subject);
+            if (! got.equals(tc.want)) {
+                System.out.printf("FAIL\n");
+                System.out.printf("  GOT:  %s\n", got);
+                System.out.printf("  WANT: %s\n", tc.want);
+            } else {
+                System.out.printf("PASS\n");
+            }
+            
+        } 
 
 	}
 	public void testStringsFirstAssignments(){
