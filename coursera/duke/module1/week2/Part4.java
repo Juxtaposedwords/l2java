da404f4e47858bdd7e1d9e52557eb00d899edf13
juxtaposedwords <juxtaposedwords@gmail.com>
2021-02-26 17:26:16 -0800

    Adding base scrape details (only chekcked for compilation).

--- coursera/duke/module1/week2/Part4.java
+++ coursera/duke/module1/week2/Part4.java
@@ -1,3 +1,11 @@
+import edu.duke.*;
+import java.io.*;
 
 public class  Part4 {
+	public void scrape() {
+		URLResource site = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+		for ( String line: site.lines()){
+		 	System.out.printf("%s\n",line);	
+		}
+	}
 }
