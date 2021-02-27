266fc03ab91614f93f772b74189a99a6b7550bef
juxtaposedwords <juxtaposedwords@gmail.com>
2021-02-26 18:24:32 -0800

    Adding a functional version of the urls. This way we enable direct testing of only scraping.

--- coursera/duke/module1/week2/Part4.java
+++ coursera/duke/module1/week2/Part4.java
@@ -1,30 +1,34 @@
 import edu.duke.*;
 import java.io.*;
+import java.util.*;
 
 public class  Part4 {
-	public String url(String input) {
-		String lowerInput =  input.toLowerCase();
-		String anchor = "href=\"";
-		int tokenIndex = lowerInput.indexOf(anchor); 
-		if (tokenIndex == -1){
-			return "";
-		}
-		input = input.substring(tokenIndex+anchor.length());
-		anchor = "\">";
-		tokenIndex =input.indexOf(anchor);
-		if (tokenIndex == -1) {
-			 System.out.printf("failed to find ending token for %s\n",input);	
-			 return "";
-		}
-		return  input.substring(0,input.indexOf(anchor));
-	}
-	public void scrape() {
-		URLResource site = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
-		for ( String line: site.lines()){
-			String lineURL =  url(line);
-			if (lineURL.toLowerCase().contains("youtube.com")) {					
-			 	System.out.printf("%s\n",lineURL);	
-			}
-		}
-	}
+    public ArrayList<String> urls(String source) {
+        ArrayList<String> output =  new ArrayList<String>();
+        URLResource site = new URLResource(source);
+        for (String line: site.lines()){
+            String lowerLine=  line.toLowerCase();
+            String anchor = "href=\"";
+            int tokenIndex = lowerLine.indexOf(anchor); 
+            if (tokenIndex == -1){
+                continue;
+            }
+            line = line.substring(tokenIndex+anchor.length());
+            anchor = "\">";
+            tokenIndex = line.indexOf(anchor);
+            if (tokenIndex == -1) {
+                 System.out.printf("failed to find ending token for %s\n",line);    
+                 continue;
+            } 
+            output.add(line.substring(0,line.indexOf(anchor)));
+        }
+        return output;
+    }
+    public void scrape() {
+        for ( String line: urls("https://www.dukelearntoprogram.com//course2/data/manylinks.html")){
+            if (line.toLowerCase().contains("youtube.com")) {
+            System.out.printf("%s\n",line);
+        }
+        }
+    }
 }
