858cabaa8899c395a0d5838bf333f5ab99fa0d48
juxtaposedwords <juxtaposedwords@gmail.com>
2021-02-26 18:06:48 -0800

    Adding working example. Next is figuring out Java tests.

--- coursera/duke/module1/week2/Part4.java
+++ coursera/duke/module1/week2/Part4.java
@@ -2,10 +2,29 @@ import edu.duke.*;
 import java.io.*;
 
 public class  Part4 {
+	public String url(String input) {
+		String lowerInput =  input.toLowerCase();
+		String anchor = "href=\"";
+		int tokenIndex = lowerInput.indexOf(anchor); 
+		if (tokenIndex == -1){
+			return "";
+		}
+		input = input.substring(tokenIndex+anchor.length());
+		anchor = "\">";
+		tokenIndex =input.indexOf(anchor);
+		if (tokenIndex == -1) {
+			 System.out.printf("failed to find ending token for %s\n",input);	
+			 return "";
+		}
+		return  input.substring(0,input.indexOf(anchor));
+	}
 	public void scrape() {
 		URLResource site = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
 		for ( String line: site.lines()){
-		 	System.out.printf("%s\n",line);	
+			String lineURL =  url(line);
+			if (lineURL.toLowerCase().contains("youtube.com")) {					
+			 	System.out.printf("%s\n",lineURL);	
+			}
 		}
 	}
 }
