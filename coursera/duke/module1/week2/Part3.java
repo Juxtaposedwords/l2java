10fa4a78cd2b3c3bd61d7980871db3fd9a583ba2
juxtaposedwords <juxtaposedwords@gmail.com>
2021-02-26 17:04:38 -0800

    Updating test and corect twoOccurences

--- coursera/duke/module1/week2/Part3.java
+++ coursera/duke/module1/week2/Part3.java
@@ -4,13 +4,19 @@ import java.io.*;
 
 public class  Part3 {
 
-    public boolean StringsFirstAssignments(String subject, String corpus){
-        if (corpus.split(subject).length >= 3){
-            return true;
+    public boolean twoOccurrences(String subject, String corpus){
+
+        String text = corpus;
+        int occurences = 0;
+        for (int i = 0; i < 2; i++){
+            if (text.indexOf(subject) == -1) {
+                return false;
+            }
+            text = text.substring(text.indexOf(subject)+1);
         }
-        return false;
+        return true;
     }
-    public String lastPart(String subject, String corpus){
+    public String lastPart( String subject,String corpus){
         String[]  parts =  corpus.split(subject,2);
         if (parts.length == 2) {
 			return parts[1];
@@ -19,14 +25,15 @@ public class  Part3 {
 	}
 	public void testing(){
 		testLastPart();
+		testTwoOccurences();
 
 	}
-	class testCase {
+	class testOccurence {
         public String description;
         public String corpus;
         public String subject;
-        public String want;
-        testCase(String description, String corpus, String subject, String want){
+        public boolean want;
+        testOccurence(String description, String corpus, String subject, boolean want){
             this.description = description;
             this.corpus = corpus;
             this.subject= subject;
@@ -37,47 +44,79 @@ public class  Part3 {
         public String description;
         public String corpus;
         public String subject;
-        public boolean want;
-        testCase(String description, String corpus, String subject, boolean want){
+        public String want;
+        testLast(String description, String corpus, String subject, String want){
             this.description = description;
             this.corpus = corpus;
             this.subject= subject;
             this.want = want;
         }
     }
-	public void testLastPart(){
-		 testCase[] tests = new testCase[] {
+	private void testLastPart(){
+		 testLast[] tests = new testLast[] {
         new testLast( 
             "subject exist in corpus many times",
-            "bodybody"
-            "dy",
-            true),
+            "abca",
+            "a",
+            "bca"),
         new testLast(
             "subject not in corpus",
-            "bodybody",
-            "flegm",
-            false),
+            "abc",
+            "d",
+            "abc"),
         new testLast(
             "subject exists in corpus once",
-            "ayebody",
-            "dy",
-            true)};
+            "abc",
+            "a",
+            "bc"),
+        new testLast(
+            "provided example",
+            "banana",
+            "an",
+            "ana"),
+        new testLast(
+            "second provide example",
+            "forest",
+            "zoo",
+            "forest")};
         for ( testLast tc: tests ) {
-            System.out.printf("Test %s:\n",tc.description);
-            System.out.printf("  Status:");
-            boolean got =  lastPart(tc.corpus.tc.subject);
-            if (! got.equals(tc.want)) {
-                System.out.printf("FAIL\n");
+            System.out.printf("Test lastPart %s:\n",tc.description);
+            String got =  lastPart(tc.subject,tc.corpus);
+            if (  ! got.equals(tc.want) ) {
+                System.out.printf("  Status: FAIL\n");
                 System.out.printf("  GOT:  %s\n", got);
                 System.out.printf("  WANT: %s\n", tc.want);
-            } else {
-                System.out.printf("PASS\n");
-            }
+            } 
             
         } 
 
 	}
-	public void testStringsFirstAssignments(){
-
-	}
+	private void testTwoOccurences(){
+        testOccurence[] tests = new testOccurence[] {
+            new testOccurence( 
+                "subject exist in corpus 2 times",
+                "abca",
+                "a",
+                true),
+            new testOccurence(
+                "subject not in corpus",
+                "abc",
+                "d",
+                false),
+            new testOccurence(
+                "subject exists in corpus once",
+                "abc",
+                "a",
+                false)};
+        for ( testOccurence tc: tests ) {
+            System.out.printf("Test twoOccurences %s:\n",tc.description);
+            boolean got =  twoOccurrences(tc.subject,tc.corpus);
+            if ( got != tc.want ) {
+                System.out.printf("  Status: FAIL\n");
+                System.out.printf("  GOT:  %s\n", got);
+                System.out.printf("  WANT: %s\n", tc.want);
+            } 
+            
+	   }
+    }
 }
