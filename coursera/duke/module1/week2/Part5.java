98442e58ba0090951acbe556615cdd48d5c833ce
Maloy Wilkes <Juxtaposedwords@gmail.com>
2021-02-27 18:27:37 -0800

    Checking in tests, as promised. (ouch)

--- coursera/duke/module1/week2/Part5.java
+++ coursera/duke/module1/week2/Part5.java
@@ -5,7 +5,7 @@ public class Part5 {
     class tuple {
         int start;
         int end;
-        tuple(int start, int end){
+        tuple( int start, int end){
             this.start = start;
             this.end = end;
         }
@@ -17,28 +17,72 @@ public class Part5 {
         dna = dna.toLowerCase();
         int startIndex = dna.indexOf(startCodon);
         if (startIndex == -1) {
-            return output;
+            return new tuple(-1,-1);
         }
         output.start = startIndex;
-        int stopIndex = dna.length()-1; // we can never have this value (not mod 3).
+        int stopIndex = 0;
         for ( String codon: endCodons ) {
-            int index = dna.indexOf(codon, startIndex+3);
-            if (index != -1 && index <= stopIndex) {
-                stopIndex = index;
-            }
+        	int index = startIndex;
+        	int instance =1;
+        	while (index != -1 ){
+        		index = dna.indexOf(codon,index+3);
+        		//System.out.printf(" a: %b b: %b c: %b\n",index != -1,(index-startIndex) %3 == 0,(index < stopIndex || stopIndex==0));
+        		if (index != -1 && (index-startIndex) %3 == 0 &&(index < stopIndex || stopIndex==0)){
+        			stopIndex = index;
+        		}  
+        	}            
         }
-        if (stopIndex == dna.length()-1) {
-            return output;
+        if (stopIndex == 0 ){
+            return new tuple(startIndex,-1);
         }
-        output.start = startIndex;
-        output.end = stopIndex;
+        output.end = stopIndex+3;
         return output;
     }
-    class testCase {
+    public class testCase {
+        String description;
         String dna;
         tuple want;
+        testCase( String description, String dna, tuple want ){
+        	this.description = description;
+        	this.dna = dna;
+        	this.want = want;
+        }
     }
     public void testFindProtein(){
-        
+		testCase[] tests = new testCase[] {
+		        new testCase(
+				"multiple pairs exists",
+				"zzzATGzzzTGAzzzATGzzzTAGzz",
+				new tuple(3,12)),
+			new testCase(
+				"happy path: no distance between",
+				"ATGTAG",
+				new tuple(0,6)),
+			new testCase(
+				"no end codon with modulo 3 distance",
+				"ATGTATga",
+				new tuple(0,-1)),
+			new testCase(
+				"first end codon not modulo 3 distance",
+				"ATGTATga",
+				new tuple(0,-1)),			
+			new testCase(
+				"no start",
+				"AAAAAATTTTTTTTTTG",
+				new tuple(-1,-1)),			
+			};
+        for ( testCase tc: tests ) {
+        	tuple got = findProtein(tc.dna);
+        	if (got.start != tc.want.start || got.end != tc.want.end ){
+        		System.out.printf("Test: %s (GOT->WANT):\n",tc.description);
+        		if (got.start != tc.want.start) {
+	        		System.out.printf("  start: %d -> %d\n",got.start,tc.want.start);
+        		}
+        		if (got.end != tc.want.end ){
+	        		System.out.printf("  end:   %d -> %d\n",got.end,tc.want.end);
+        		}
+        	}
+        }
+
     }
 }
