8d850e630510af3257c78b2b025ea45b23f947b7
Juxtaposed <juxtaposedwords@gmail.com>
2022-07-10 11:45:04 -0700

    adding unit tte4st working example

--- misc/hello_world/helloworld/src/App.java
+++ misc/hello_world/helloworld/src/App.java
@@ -1,12 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Circle one = new Circle(10, 5, 5);
-        Circle two = new Circle(5);
-        Circle three = new Circle();
+    private Circle one, two, three;
 
-        System.out.println(one.toString());
-        System.out.println(two.toString());
-        System.out.println(three.toString());
+    public App() {
+        one = new Circle(10, 5, 5);
+        two = new Circle(5);
+        three = new Circle();
+    }
+
+    public static void main(String[] args) throws Exception {
+        App foo = new App();
+
+        System.out.println(foo.one.toString());
+        System.out.println(foo.two.toString());
+        System.out.println(foo.three.toString());
 
         int i = 0;
         while (i < 10) {
@@ -14,4 +20,10 @@ public class App {
             i++;
         }
     }
+
+    @Override
+    public String toString() {
+        return String.format("circle 1: %s circle 2: %s circle 3: %s", one, two, three);
+    }
+
 }
