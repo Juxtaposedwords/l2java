c87e4de40f08694eb8ea31484b32b630fcac67dd
Juxtaposed <juxtaposedwords@gmail.com>
2022-07-10 12:20:14 -0700

    Moving to a single vscode window per project

---
+++ misc/helloworld/src/AppTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+
+    @Test
+    public void testToString() {
+        assertEquals(new App().toString(), "circle 1: Circle [radius=10.0, xCenter=5.0, yCenter=5.0] circle 2: Circle [radius=5.0, xCenter=0.0, yCenter=0.0] circle 3: Circle [radius=0.0, xCenter=0.0, yCenter=0.0]");
+
+    }
+}
